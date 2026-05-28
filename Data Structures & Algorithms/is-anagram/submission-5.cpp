576d81a0b655b7ae518a1862e032c4c7ea131313
Krunal Kadtan <149938129+KrunalKadtan@users.noreply.github.com>
2026-05-28 11:01:55 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(s != t) return false;
+        
+        set s_s(s.begin(), s.end());
+        set s_t(t.begin(), t.end());
+
+        if (s_s == s_t) return true;
+
+        return false;
+    }
+};
