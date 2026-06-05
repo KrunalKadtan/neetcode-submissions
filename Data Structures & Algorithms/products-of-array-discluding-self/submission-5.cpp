6922a7801634d8896d9ff2da4635b53b1e50cad2
Krunal Kadtan <149938129+KrunalKadtan@users.noreply.github.com>
2026-06-05 11:38:36 +0530

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,37 @@
+class Solution {
+   public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        int prod = 1;
+
+        for(int n : nums) {
+            prod *= n;
+        }
+
+        vector<int> ans(nums.size(), prod);
+
+        for (int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                ans[i] /= nums[i];
+            } else {
+                prod = 1;
+                int count = 0;
+                for (int j = 0; j < nums.size(); j++) {
+                    if (i == j) {
+                        count++;
+                        continue;
+                    } else {
+                        prod *= nums[j];
+                    }
+                }
+                if (count == nums.size()) {
+                    ans[i] = 0;
+                } else {
+                    ans[i] = prod;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
